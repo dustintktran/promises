e7e560eabc0727eceba155868f8e266f6266b5d6
dustintktran <dustintktran@gmail.com>
2019-04-30 10:45:48 -0700

    fin

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,14 +10,53 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
 
+var fetchProfileAndWriteToFile = function (readFilePath, writeFilePath) {
 
+  return new Promise((resolve, reject) => {
+    fs.readFile(readFilePath, 'utf8', (err, success) => {
+      if (err) {
+        reject(err)
+      } else {
+        let first = success.split("\n")[0]
+        resolve(first) // danthareja
+      }
+    })
+  })
+    .then((data) => {
+      return new Promise((resolve, reject) => {
+        request({
+          url: `https://api.github.com/users/${data}`,
+          headers: {'Accept': 'application/vnd.github.v3+json'}
+        }, function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            // console.log(response)
+            resolve(response);
+          }
+        });
+      })
+    })// danthareja)
+  .then((data) => {
+    let body = data.body;
+    return new Promise((resolve, reject) => {
+      fs.writeFile(writeFilePath, body, 'utf8', (err) => {
+        if (err){
+          reject(err)
+        } else {
+          resolve(body);
+        }
+      })
+    })
+  }) //200
 
-var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
 };
 
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
+
+
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -9,13 +9,35 @@ var request = require('request');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
+var pluckFirstLineFromFileAsync = function (filePath) {
   // TODO
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, success) => {
+      if (err) {
+        reject(err)
+      } else {
+        let first = success.split("\n")[0]
+        resolve(first)
+      }
+    })
+  })
 };
 
+
+
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCodeAsync = function(url) {
+var getStatusCodeAsync = function (url) {
   // TODO
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response.statusCode);
+      }
+    });
+
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
