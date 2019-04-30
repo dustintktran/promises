e4dd4482baaebaac1fafba4802e096f7b73f10e7
dustintktran <dustintktran@gmail.com>
2019-04-30 11:15:00 -0700

    ksfeuhidsf

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,10 +7,40 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var fs = require('fs');
+var Promise = require('bluebird');
 
+var combineFirstLineOfManyFiles = function (filePaths, writePath) {
+  // TODO
+  let array = filePaths.map(item => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(item, 'utf8', (err, success) => {
+        if (err) {
+          reject(err)
+        } else {
+          let first = success.split("\n")[0]
+          resolve(first);
+        }
+      })
+    })
+  })
 
-var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+  return Promise.all(array)
+    .then(data => data.join('\n'))
+    .then(data => {
+      console.log("our data!!!", data)
+      return new Promise((resolve, reject) => {
+        fs.writeFile(writePath, data, 'utf8', (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(data)
+          }
+        })
+      })
+
+    })
+
 };
 
 // Export these functions so we can unit test them
